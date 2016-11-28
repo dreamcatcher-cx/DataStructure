dd9aaa54db5c44843a2e46d8f0a6846fb143e0b6
chengxiao <chengxiao@qtonecloud.cn>
2016-11-28 11:27:43 +0800

    希尔排序小调整

--- src/sortdemo/ShellSort.java
+++ src/sortdemo/ShellSort.java
@@ -8,19 +8,25 @@ import java.util.Arrays;
 public class ShellSort {
     public static void main(String []args){
         int []arr ={1,4,2,7,9,8,3,6};
-        sort1(arr);
+        sort(arr);
         System.out.println(Arrays.toString(arr));
+        int []arr1 ={1,4,2,7,9,8,3,6};
+        sort1(arr1);
+        System.out.println(Arrays.toString(arr1));
     }
 
     /**
-     * 希尔排序
+     * 希尔排序 针对有序序列在插入时采用交换法
      * @param arr
      */
-    public static void sort1(int []arr){
+    public static void sort(int []arr){
+        //增量gap，并逐步缩小增量
        for(int gap=arr.length/2;gap>0;gap/=2){
+           //
            for(int i=gap;i<arr.length;i++){
                int j = i;
                while(j-gap>=0 && arr[j]<arr[j-gap]){
+                   //插入排序采用交换法
                    swap(arr,j,j-gap);
                    j-=gap;
                }
@@ -29,27 +35,25 @@ public class ShellSort {
     }
 
     /**
-     * 希尔排序
+     * 希尔排序 针对有序序列在插入时采用移动法。
      * @param arr
      */
-    public static void sort(int []arr){
-        int gap = arr.length/3+1;
-        while(gap>0){
-            //分为gap-1组,逐组进行简单插入排序
-            for(int i=0;i<gap;i++){
-                for(int j=gap+i;j<arr.length;j+=gap){
-                    int k = j;
-                    if(arr[k]<arr[k-gap]){
-                        while(k-gap>=0  && arr[k]<arr[k-gap]){
-                            arr[k] = arr[k]+arr[k-gap];
-                            arr[k-gap] = arr[k] - arr[k-gap];
-                            arr[k] = arr[k] - arr[k-gap];
-                            k-=gap;
-                        }
+    public static void sort1(int []arr){
+        //增量gap，并逐步缩小增量
+        for(int gap=arr.length/2;gap>0;gap/=2){
+            //从第gap个元素，逐个对其所在组进行直接插入排序操作
+            for(int i=gap;i<arr.length;i++){
+                int j = i;
+                int temp = arr[j];
+                if(arr[j]<arr[j-gap]){
+                    while(j-gap>=0 && arr[j]<arr[j-gap]){
+                        //移动法
+                        arr[j] = arr[j-gap];
+                        j-=gap;
                     }
+                    arr[j] = temp;
                 }
             }
-            gap/=2;
         }
     }
     /**
